0d5e9274d520af0b7978949ca4b20eeb1c6f15b5
1337Farhan <jx@outlook.cl>
2023-05-07 16:34:32 +0300

    updated key name

--- CryptiWeb/settings.py
+++ CryptiWeb/settings.py
@@ -132,6 +132,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # AWS creds
 BUCKET_NAME = "crypti-predict"
-AWS_S3_ACCESS_KEY_ID = os.environ["AWS_S3_ACCESS_KEY_ID"]
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 AWS_DEFAULT_REGION = os.environ["AWS_DEFAULT_REGION"]
